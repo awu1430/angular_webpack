eaf92aff04672d70f922e8a30a4636e36127aa4b
awu1430 <junwuyh@yahoo.com>
2016-10-22 21:12:01 -0500

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,7 @@
+angular.module('dashboard', []);
+
+
+
+require('./directives/yep-nope.directive.js');
+require('./services/github-status.service.js');
+require('./controllers/dashboard.controller.js');
